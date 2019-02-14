ac40dd90d292e60ed5caff3bcbf5d1b79b6ec7ea
izarc6 <izar.castorina@gmail.com>
2019-02-14 11:28:24 +0100

    Added Makefile & added functions to bloques.c

--- bloques.c
+++ bloques.c
@@ -1,11 +1,72 @@
-#include <stdio.h>
+#include "bloques.h"
+static int descriptor = 0;
 
-#define BLOCKSIZE = 1024    // Tamaño de bloque lògico
+int bmount(const char *camino)
+{
+    int fd = open(camino, O_RDWR | O_CREAT, 666);
+    if (fd == -1)
+    {
+        fprintf(stderr, "ERROR fallo lect . \n");
+    }
+    descriptor = fd;
 
-int bmount(const char *camino); // Mount volume
-int bumount();                  // Unmount volume
-int bwrite(unsigned int nbloque, const void *buf);  // Write block
-int bread(unsigned int nbloque, void *buf)  // Read block
+    return fd;
+}
 
-// NOTA: Cada llamada a sistema tiene que tener gestion/control de errores a seguir
+int bumount()
+{ //liberar descriptor de ficheros
+    int fc = close(descriptor);
+    if (fc == -1)
+    {
+        //Error
+        fprintf(stderr, "ERROR: fallo al cerrar . \n");
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
+int bwrite(unsigned int nbloque, const void *buf)
+{ //escriure un bloc, el indicat per nbloque, s'sescriu *buf
+    //bwrite retorna el nº de bytes que s'ha escrit o -1 si error
+
+    off_t desplazamiento = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
+    if (desplazamiento == -1)
+    {
+        //Error
+        fprintf(stderr, "ERROR: fallo al buscar bloque . \n");
+        return -1;
+    }
+    else
+    {
+        int bescritos = write(descriptor, buf, BLOCKSIZE);
+        if (bescritos == -1)
+        {
+            fprintf(stderr, "ERROR: fallo al escribir el bloque . \n");
+        }
+        return bescritos;
+    }
+}
+
+int bread(unsigned int nbloque, void *buf)
+{
+
+    off_t desplazamiento = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
+    if (desplazamiento == -1)
+    {
+        //Error
+        fprintf(stderr, "ERROR: fallo al buscar bloque . \n");
+        return -1;
+    }
+    else
+    {
+        int bleidos = read(descriptor, buf, BLOCKSIZE);
+        if (bleidos == -1)
+        {
+            fprintf(stderr, "ERROR: fallo al leer el bloque . \n");
+        }
+        return bleidos;
+    }
+}
