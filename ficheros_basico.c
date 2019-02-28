5f52b415fd21c73e5c815b48d83cb7a139f467b8
izarc6 <izar.castorina@gmail.com>
2019-02-28 11:03:01 +0100

    No sé qué he cambiado, la verdad. Sorry

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -186,26 +186,24 @@ int liberar_bloque(unsigned int nbloque) {
 
 int escribir_inodo(unsigned int ninodo, struct inodo inodo) {
   //Escribe el contenido de una variable del tipo struct inodo en un determinado inodo del array de inodos
-  struct superbloque SB = bread(0,&SB);
-  if (SB == 0) {
+  if (bread(0,&SB) == 0) {  
     fprintf(stderr, "Error en ficheros_basico.c escribir_inodo() --> %d: %s\n", errno, strerror(errno));
     return -1;
   }
   unsigned int posInodo = SB.posPrimerBloqueAI + (ninodo * INODOSIZE) / BLOCKSIZE;
   struct inodo inodos[BLOCKSIZE/INODOSIZE];
   bread(posInodo, &inodos); // Leemos el bloque del inodo
-  return bwrite(inodos[ninodo %(BLOCKSIZE/INODOSIZE)],&inodo);
+  return bwrite((ninodo %(BLOCKSIZE/INODOSIZE)),&inodo);
 }
 
 int leer_inodo(unsigned int ninodo, struct inodo *inodo) {
-  struct superbloque SB = bread(0,&SB);
-  if (SB == 0) {
+  if (bread(0,&SB) == 0) {  
     fprintf(stderr, "Error en ficheros_basico.c escribir_inodo() --> %d: %s\n", errno, strerror(errno));
     return -1;
   }
   unsigned int posInodo = SB.posPrimerBloqueAI + (ninodo * INODOSIZE) / BLOCKSIZE;
   struct inodo inodos[BLOCKSIZE/INODOSIZE];
-  return bread(inodos[ninodo %(BLOCKSIZE/INODOSIZE)],&inodo);
+  return bread((ninodo %(BLOCKSIZE/INODOSIZE)),&inodo);
 }
 
 int reservar_inodo(unsigned char tipo, unsigned char permisos) {
