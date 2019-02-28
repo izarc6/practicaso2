c3d3522e845712e670ad4d128757ee016a07a275
Andreu López Cortès <andreulopess@YoMumma69.local>
2019-02-28 10:10:39 +0100

    Añadido escribir/leer inodos en clase mientras estaba *solo* con el Izar

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -130,7 +130,8 @@ int escribir_bit(unsigned int nbloque, unsigned int bit) {
 }
 
 unsigned char leer_bit(unsigned int nbloque) {
-  return NULL;
+
+
 }
 
 int reservar_bloque() {
@@ -160,11 +161,20 @@ int liberar_bloque(unsigned int nbloque) {
 }
 
 int escribir_inodo(unsigned int ninodo, struct inodo inodo) {
-  return 0;
+  //Escribe el contenido de una variable del tipo struct inodo en un determinado inodo del array de inodos
+  struct superbloque SB = bread(0,&SB);
+  if (SB == 0) {
+    fprintf(stderr, "Error en ficheros_basico.c escribir_inodo() --> %d: %s\n", errno, strerror(errno));
+    return -1;
+  }
+  unsigned int posInodo = SB.posPrimerBloqueAI + (ninodo * INODOSIZE) / BLOCKSIZE;
+  struct inodo inodos[BLOCKSIZE/INODOSIZE];
+  bread(posInodo, &inodos); // Leemos el bloque del inodo
+  return bwrite(inodos[ninodo %(BLOCKSIZE/INODOSIZE)],&inodo);
 }
 
 int leer_inodo(unsigned int ninodo, struct inodo *inodo) {
-  return 0;
+  
 }
 
 int reservar_inodo(unsigned char tipo, unsigned char permisos) {
