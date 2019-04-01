cdc1df194480218e380e1b0304cd7f3cf6a7adde
Andreu López Cortès <andreulopess@YoMumma69.local>
2019-04-01 15:43:11 +0200

    truncar bo festtt

--- truncar.c
+++ truncar.c
@@ -3,36 +3,18 @@
 #include "ficheros.h"
 
 
+
 int main(int argc, char **argv) {
-	void *nombre_fichero = argv[1];
+
 	int ninodo = atoi(argv[2]);
 	int nbytes = atoi(argv[3]);
 	//Validacion de sintaxis
-	if(argc==4){
-	//montar dispositivo
-		if(bmount(nombre_fichero)==-1){
-			printf("Error de fichero");
-		}else{
-			//llamada a mi_truncar_f(ninodo, nbytes)
-			mi_truncar_f(ninodo,nbytes);
-		}
-	//desmontar dispositivo
-		int bum;
-		bum=bumount();
-		if(bum==-1){
-			printf("Error en el close del fitxer\n");
-			return -1;
-		} else {
-			return 0;
-		}
-	}else{
-			printf("Sintaxis: ./truncar <nombre_dispositivo> <ninodo> <nbytes>\n");
-
-	}
-
-
+  if (argc != 4) {
+    printf("Sintaxis: truncar <nombre_dispositivo> <ninodo> <permisos>\n");
+    return -1;
+  }
+  
 
 
 
 }
-
